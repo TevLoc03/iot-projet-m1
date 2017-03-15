0424fba9b484a26dbc2b4966fb082554ab609a54
TevBarbaRossa <t.locandro@gmail.com>
2017-03-15 15:24:22 +0100

    clavier + mini design bootstrap

--- main.js
+++ main.js
@@ -8,7 +8,10 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 480, height: 320})
+  win = new BrowserWindow({
+    fullscreen: true
+    //width: 480, height: 320
+  })
 
   // and load the index.html of the app.
   win.loadURL(url.format({
