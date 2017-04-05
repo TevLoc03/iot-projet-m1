08cc57387a16ae31c9a6b7d558b4e3f2b48d21c7
TevBarbaRossa <t.locandro@gmail.com>
2017-04-05 16:28:00 +0200

    css color

--- main.js
+++ main.js
@@ -9,10 +9,10 @@ let win
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({
-    //fullscreen: true,
+    fullscreen: true,
     skipTaskbar: true,
     autoHideMenuBar: true,
-    width: 480, height: 320
+    //width: 480, height: 320
   })
 
   // and load the index.html of the app.
@@ -23,7 +23,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  //win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
