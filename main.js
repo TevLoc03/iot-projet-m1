396e88d0c35a04d8b0bc574555ce93dc23b22cf1
TevBarbaRossa <t.locandro@gmail.com>
2017-04-05 15:56:23 +0200

    btn retour

--- main.js
+++ main.js
@@ -9,10 +9,10 @@ let win
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({
-    fullscreen: true,
+    //fullscreen: true,
     skipTaskbar: true,
-    autoHideMenuBar: true
-    //width: 480, height: 320
+    autoHideMenuBar: true,
+    width: 480, height: 320
   })
 
   // and load the index.html of the app.
